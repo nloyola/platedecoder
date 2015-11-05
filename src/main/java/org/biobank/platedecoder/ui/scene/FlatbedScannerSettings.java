97b97167690d3a7d47e05caa8ade4bf847322a04
Nelson Loyola <nloyola@gmail.com>
2015-11-05 15:08:19 -0700

    Working on settings

---
+++ src/main/java/org/biobank/platedecoder/ui/scene/FlatbedScannerSettings.java
@@ -0,0 +1,29 @@
+package org.biobank.platedecoder.ui.scene;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javafx.scene.Node;
+
+public class FlatbedScannerSettings extends SceneRoot {
+
+   @SuppressWarnings("unused")
+   private static final Logger LOG = LoggerFactory.getLogger(FlatbedScannerSettings.class);
+
+   public FlatbedScannerSettings() {
+      super("Flatbed scanner setings");
+   }
+
+   @Override
+   protected Node creatContents() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public void onDisplay() {
+      // TODO Auto-generated method stub
+
+   }
+
+}
