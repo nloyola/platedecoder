0c4791e1e3fa3917f9aa9c842ab52520e283a6ce
Nelson Loyola <nloyola@gmail.com>
2015-12-01 18:13:26 -0700

    Can now distribute with native libraries.
    Invoking gradle with distZip creates a runnable JAR for Linux and MS
    Windows.

--- src/main/java/org/biobank/platedecoder/dmscanlib/LibraryLoader.java
+++ src/main/java/org/biobank/platedecoder/dmscanlib/LibraryLoader.java
@@ -5,36 +5,41 @@ import org.slf4j.LoggerFactory;
 
 public class LibraryLoader {
 
-    @SuppressWarnings("unused")
-    private static final Logger LOG = LoggerFactory.getLogger(LibraryLoader.class);
+   @SuppressWarnings("unused")
+   private static final Logger LOG = LoggerFactory.getLogger(LibraryLoader.class);
 
-    private static final boolean IS_MS_WINDOWS = System.getProperty("os.name").startsWith("Windows");
+   private static final boolean IS_MS_WINDOWS = System.getProperty("os.name").startsWith("Windows");
 
-    private static final boolean IS_LINUX = System.getProperty("os.name").startsWith("Linux");
+   private static final boolean IS_LINUX = System.getProperty("os.name").startsWith("Linux");
 
-    private static final boolean IS_ARCH_64_BIT = System.getProperty("os.arch").equals("amd64");
+   private static final boolean IS_ARCH_64_BIT = System.getProperty("os.arch").equals("amd64");
 
-    /**
-     * Loads the native library for scanning and decoding.
-     */
-    public static void load() {
-        //LOG.debug("java.library.path: {}", System.getProperty("java.library.path"));
+   /**
+    * Loads the native library for scanning and decoding.
+    */
+   public static void load() {
+      //LOG.debug("java.library.path: {}", System.getProperty("java.library.path"));
 
-        if (IS_MS_WINDOWS) {
+      try {
+         if (IS_MS_WINDOWS) {
             System.loadLibrary("dmscanlib");
-        } else if (IS_LINUX && IS_ARCH_64_BIT){
+         } else if (IS_LINUX && IS_ARCH_64_BIT) {
             System.loadLibrary("dmscanlib64");
-        }
-    }
+         }
+      } catch (UnsatisfiedLinkError e) {
+         System.err.println("Could not load native code library.\n" + e);
+         System.exit(1);
+      }
+   }
 
-    /**
-     * Returns true if running on MS Windows.
-     *
-     * @return {@code true} if the operating system is running MS Windows.
-     */
-    public static boolean runningMsWindows() {
-        return IS_MS_WINDOWS;
-    }
+   /**
+    * Returns true if running on MS Windows.
+    *
+    * @return {@code true} if the operating system is running MS Windows.
+    */
+   public static boolean runningMsWindows() {
+      return IS_MS_WINDOWS;
+   }
 
 
 }
