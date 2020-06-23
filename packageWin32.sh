fbc937d2104c4cf23bb5688abdc83786a454d1ff
Nelson Loyola <nloyola@gmail.com>
2020-06-23 16:44:47 +0200

    Can now build natively for Windows

---
+++ packageWin32.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cp target/lib/dmscanlib.dll target/platedecoder/lib
+cp target/lib/libglog.dll target/platedecoder/
+cp target/lib/opencv_core248.dll target/platedecoder/
+cp target/lib/opencv_highgui248.dll target/platedecoder/
+cp target/lib/opencv_imgproc248.dll target/platedecoder/
+cp target/lib/OpenThreadsWin32.dll target/platedecoder/
